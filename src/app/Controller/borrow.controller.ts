860309f892ee410bd9624099ab9f6362757c75b0
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-06-30 22:02:25 +0600

    add get fuction for borrow get with aggregate

--- src/app/Controller/borrow.controller.ts
+++ src/app/Controller/borrow.controller.ts
@@ -78,4 +78,49 @@ borrowRoutes.post(
   }
 );
 
+borrowRoutes.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const summary = await Borrow.aggregate([
+        {
+          $group: {
+            _id: "$book",
+            totalQuantity: { $sum: "$quantity" },
+          },
+        },
+        {
+          $lookup: {
+            from: "books", 
+            localField: "_id",
+            foreignField: "_id",
+            as: "bookDetails",
+          },
+        },
+        {
+          $unwind: "$bookDetails",
+        },
+        {
+          $project: {
+            _id: 0,
+            book: {
+              title: "$bookDetails.title",
+              isbn: "$bookDetails.isbn",
+            },
+            totalQuantity: 1,
+          },
+        },
+      ]);
 
+      sendResponse({
+        res,
+        statusCode: 200,
+        success: true,
+        message: "Borrowed books summary retrieved successfully",
+        data: summary,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
